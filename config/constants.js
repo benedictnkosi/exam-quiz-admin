97e1f89bb2c9b23fb7d88b60feb9c143b482e1af
benedictnkosi <nkosi.benedict@gmail.com>
2025-03-09 19:55:30 +0200

    Update API host to production Vercel deployment

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
-//const API_HOST = 'https://api.examquiz.co.za'
-const API_HOST = 'http://localhost:3000' // Uncomment for local development
+const API_HOST = 'https://exam-quiz-admin.vercel.app'
+//const API_HOST = 'http://localhost:3000' // Uncomment for local development
 
 const API_BASE_URL = `${API_HOST}/api`
 const IMAGE_BASE_URL = `https://ozhhkdhtddoznswtplbx.supabase.co/storage/v1/object/public/question_images/`
